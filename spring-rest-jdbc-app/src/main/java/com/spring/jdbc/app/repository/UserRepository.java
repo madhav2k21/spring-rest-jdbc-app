fdd1dd8cae118e50519dc21a93f6950d9b3d1434
madhav <madhav.anupoju.2k21@gmail.com>
2021-06-04 13:12:07 +0530

    Added 3 approaches to iterate ResultSet
    1. Custom Extractor
    2. Anonymous Inner type
    3. Java 8 Lambda expression

--- spring-rest-jdbc-app/src/main/java/com/spring/jdbc/app/repository/UserRepository.java
+++ spring-rest-jdbc-app/src/main/java/com/spring/jdbc/app/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.spring.jdbc.app.repository;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 
 import com.spring.jdbc.app.common.DBQueries;
@@ -30,9 +33,30 @@ public class UserRepository {
 		//Approach-1: Using Custom extractor class: End
 		
 		//Approach-2: Using anonymous inner class start
+		/*
+		List<Users> users = new ArrayList<>();
+		Object[] params = {};
+		jdbcTemplate.query(DBQueries.SELECT_ALL_USERS, new ResultSetExtractor<List<Users>>() {
+
+			@Override
+			public List<Users> extractData(ResultSet rs) throws SQLException, DataAccessException {
+
+				while (rs.next()) {
+					Users user = new Users();
+					user.setId(rs.getInt("ID"));
+					user.setName(rs.getString("NAME"));
+					user.setLocation(rs.getString("LOCATION"));
+					users.add(user);
+				}
+
+				return users;
+			}
+		}, params);
+		*/
 		//Approach-2: Using anonymous inner class end
 
 		//Approach-3: Using java8 Lambda expression start (recommended approach)
+		
 		List<Users> users = new ArrayList<>();
 		
 		Object[] params = {};
@@ -49,7 +73,8 @@ public class UserRepository {
 			
 			return users;
 		}, params);
-		//Approach-2: Using java8 Lambda expression End
+		
+		//Approach-3: Using java8 Lambda expression End
 		
 		
 
@@ -65,14 +90,34 @@ public class UserRepository {
 		*/
 		//Approach-1: Using Custom extractor class: End
 		
+		
 		//Approach-2: Using anonymous inner class start
+		
+		/*
+		
+		Object[] params = { id };
+		Users user = jdbcTemplate.query(DBQueries.SELECT_USER_BY_ID, new ResultSetExtractor<Users>() {
+			Users user = new Users();
+			@Override
+			public Users extractData(ResultSet rs) throws SQLException, DataAccessException {
+				while (rs.next()) {
+					user.setId(rs.getInt("ID"));
+					user.setName(rs.getString("NAME"));
+					user.setLocation(rs.getString("LOCATION"));
+				}
+				return user;
+			}
+		}, params);
+		
+		*/
 		//Approach-2: Using anonymous inner class end
 
 		//Approach-3: Using java8 Lambda expression start (recommended approach)
+		
 		Object[] params = { id };
-		
+
 		Users user = new Users();
-		
+
 		jdbcTemplate.query(DBQueries.SELECT_USER_BY_ID, (rs) -> {
 			while (rs.next()) {
 				user.setId(rs.getInt("ID"));
@@ -81,7 +126,8 @@ public class UserRepository {
 			}
 			return user;
 		}, params);
-		//Approach-2: Using java8 Lambda expression End
+
+		//Approach-3: Using java8 Lambda expression End
 		
 		return user;
 	}
